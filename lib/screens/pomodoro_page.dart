161f9a6fed547578ee60a8ae6121b073363f9baa
imarivero <imanol.rivero7@gmail.com>
2022-11-11 07:55:50 -0600

    Correccion de errores en pomodoro_page, uso de const e importacion publica en en yaml

--- lib/screens/pomodoro_page.dart
+++ lib/screens/pomodoro_page.dart
@@ -2,21 +2,23 @@ import 'dart:async';
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+
 import 'package:percent_indicator/percent_indicator.dart';
-import 'ScreenSize_reduces.dart';
+// TODO: De donde viene screenSize_reduces?
+// import 'ScreenSize_reduces.dart';
 //import 'package:flutter/rendering.dart';
 
 class PomodoroPage extends StatefulWidget {
-  PomodoroPage({Key? key}) : super(key: key);
+  const PomodoroPage({Key? key}) : super(key: key);
 
   @override
   State<PomodoroPage> createState() => _PomodoroPageState();
 }
 
 class _PomodoroPageState extends State<PomodoroPage> {
-  double _progreso = 0;
+  final double _progreso = 0;
   static int _minutos = 25;
-  int _segundos = _minutos * 60;
+  final int _segundos = _minutos * 60; 
   static const maxSeconds = 25;
   int seconds = maxSeconds;
   Timer? tiempo;
@@ -29,7 +31,7 @@ class _PomodoroPageState extends State<PomodoroPage> {
       child: Scaffold(
         body: Container(
           //height: auxi_alto,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               gradient: LinearGradient(
                   colors: [Color(0xff1542bf), Color(0xff51a8ff)],
                   begin: FractionalOffset(0.5, 1))),
@@ -38,7 +40,7 @@ class _PomodoroPageState extends State<PomodoroPage> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Padding(
+                const Padding(
                   padding: EdgeInsets.only(top: 10.0),
                   child: Text(
                     "Pomodoro",
@@ -46,17 +48,17 @@ class _PomodoroPageState extends State<PomodoroPage> {
                   ),
                 ),
                 Expanded(
-                    child: CircularPercentIndicator(
-                  percent: _progreso,
-                  animation: true,
-                  animateFromLastPercent: true,
-                  radius: 140.0,
-                  lineWidth: 20.0,
-                  progressColor: Colors.white,
-                  center: Text("25:00",
-                      style: TextStyle(color: Colors.white, fontSize: 60.0)),
-                )),
-                SizedBox(
+                  child: CircularPercentIndicator(
+                    percent: _progreso,
+                    animation: true,
+                    animateFromLastPercent: true,
+                    radius: 140.0,
+                    lineWidth: 20.0,
+                    progressColor: Colors.white,
+                    center: const Text("25:00",
+                        style: TextStyle(color: Colors.white, fontSize: 60.0)),
+                  )),
+                const SizedBox(
                   height: 30.0,
                 ),
                 Expanded(
@@ -67,33 +69,33 @@ class _PomodoroPageState extends State<PomodoroPage> {
                         onPressed: (() {
                           print("Boton de pausa");
                         }),
-                        child: Icon(
+                        backgroundColor: Colors.grey,
+                        child: const Icon(
                           Icons.pause,
                           color: Colors.white,
                         ),
-                        backgroundColor: Colors.grey,
                       ),
                       FloatingActionButton(
                         onPressed: (() {
                           _iniciarPomodoro();
                           //print("Botón de play");
                         }),
-                        child: Icon(
+                        backgroundColor: Colors.grey,
+                        child: const Icon(
                           Icons.play_arrow,
                           color: Colors.white,
                         ),
-                        backgroundColor: Colors.grey,
                       ),
                       FloatingActionButton(
                         onPressed: (() {
                           _detenerPomodoro();
                           print("Botón de stop");
                         }),
-                        child: Icon(
+                        backgroundColor: Colors.grey,
+                        child: const Icon(
                           Icons.stop,
                           color: Colors.white,
                         ),
-                        backgroundColor: Colors.grey,
                       ),
                     ],
                   ),
